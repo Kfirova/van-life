909f3c88ddc18b69e898984a5a1e1715e6747456
Kfir <79320202+Kfirova@users.noreply.github.com>
2023-12-14 10:14:07 +0200

    edit van component and change icon

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from './components/About';
 import Home from "./components/Home";
@@ -9,6 +10,15 @@ import Vans from "./components/Vans";
 
 
 export default function App() {
+
+const [vans, setVans] = React.useState([])
+
+React.useEffect(() => {
+  fetch('/api/vans')
+    .then(res => res.json())
+      .then(res => setVans(res))
+}, [])
+
   return (
     <BrowserRouter>
       <NavBar />
@@ -16,7 +26,7 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
-          <Route path='/vans' element={<Vans />}/>
+          <Route path='/vans' element={<Vans vans={vans}/>}/>
   
         </Routes>
       
--- src/components/Vans.js
+++ src/components/Vans.js
@@ -1,5 +1,53 @@
+import { Link } from "react-router-dom"
+
+
+
 export default function Vans(props) {
+    let list
+if(props.vans.vans) {
+     list = props.vans.vans.map(e => 
+        <div className="van-container">
+        <Link>
+            <img className="van-image" src={e.imageUrl} alt={e.name}/>
+        </Link>
+            
+            <p className="van-name-price">
+                <span className='van-name'>{e.name}</span>
+                <span className="van-price">${e.price}</span>
+            </p>
+            <p className={`van-type ${e.type}`}>
+                {e.type}
+            </p>
+        </div>
+    )
+}
+
+
     return(
-        <h1>Vans pages goes here</h1>
+        <main >
+            <h2 className="vans-title">Explore our van options</h2>
+            <div className="vans-type-link-container">
+                <Link className="vans-type-link">
+                    Simple
+                </Link>
+
+                <Link className="vans-type-link">
+                    Luxury
+                </Link>
+
+                <Link className="vans-type-link">
+                    Rugged
+                </Link>
+                <Link className="vans-type-link">
+                    Clear Filters
+                </Link>
+            </div>
+           
+          
+            <article className="vans-container">
+            {list}
+            </article>
+          
+        </main>
     )
 }
